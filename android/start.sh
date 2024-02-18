5aa8724b364cd6909a430c38e1ebfe2a9b74c94d
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-02-18 11:30:35 -0800

    Create start.sh

---
+++ android/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if ! command -v wget &> /dev/null; then
+    echo "wget no está instalado. Instalando..."
+    pkg install wget -y
+fi
+
+if [ ! -f "servidor.zip" ]; then
+    echo "Descargando servidor.zip..."
+    wget https://github.com/UserZero075/DownFast/blob/main/android/servidor.zip
+    unzip servidor.zip
+fi
+
+cd servidor_\(Termux\)/
+
+if ! command -v node &> /dev/null; then
+    echo "Node.js no está instalado. Instalando..."
+    pkg install nodejs -y
+fi
+
+termux-setup-storage
+echo -e "\033[32mServidor interno de la App habilitado\033[0m"
+node index.js

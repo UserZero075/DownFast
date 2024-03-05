1360368133e1499662bf0a6df4557db64ddbfe09
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-03-04 21:16:14 -0500

    Update start.sh

--- android/start.sh
+++ android/start.sh
@@ -5,13 +5,13 @@ if ! command -v wget &> /dev/null; then
     apt update && apt install wget && apt upgrade && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
 fi
 
-if [ ! -f "servidor_v0.2.0.zip" ]; then
+if [ ! -f "servidor_v0.2.1.zip" ]; then
     echo "Descargando servidor.zip..."
-    wget https://raw.githubusercontent.com/UserZero075/DownFast/main/android/servidor_v0.2.0.zip
-    unzip -o servidor_v0.2.0.zip
+    wget https://raw.githubusercontent.com/UserZero075/DownFast/main/android/servidor_v0.2.1.zip
+    unzip -o servidor_v0.2.1.zip -d servidor_v0.2.1
 fi
 
-cd servidor_\(Termux\)/
+cd servidor_v0.2.1
 
 if ! command -v node &> /dev/null; then
     echo "Node.js no está instalado. Instalando..."

77e2a13d5eb11609d31f118877877ae4804c2af4
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-04-21 21:19:44 -0400

    Update start.sh

--- android/VPN/start.sh
+++ android/VPN/start.sh
@@ -22,6 +22,8 @@ if ! command -v node &> /dev/null; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    unset PREFIX
+    nvm use --delete-prefix v20.12.2
     nvm install 20.12.2
 fi
 

f8ab601feb8c00b85827cb93c07ffc85d53068d2
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-04-21 21:11:07 -0400

    Update start.sh

--- android/VPN/start.sh
+++ android/VPN/start.sh
@@ -20,6 +20,7 @@ required_version="v20.12.2"
 if ! command -v node &> /dev/null; then
     echo "Node.js no está instalado. Instalando..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     nvm install 20.12.2
 fi

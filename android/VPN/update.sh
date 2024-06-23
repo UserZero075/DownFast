2909b22d5f5075c8d4ecf481a2819e4d3ea6d64b
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-06-23 09:16:29 -0400

    Update update.sh

--- android/VPN/update.sh
+++ android/VPN/update.sh
@@ -5,10 +5,11 @@ if ! command -v wget &> /dev/null; then
     apt update -y && apt install -y wget && apt upgrade -y && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -y && bash InstallTools.sh -y
 fi
 
+mv VPN_0.5.8_android.zip VPN_0.5.9_android.zip
+
 cd VPN/
 
 wget -O VPN/index.js https://raw.githubusercontent.com/UserZero075/DownFast/main/android/VPN/index.js
-
 
 echo -e "\033[32mVPN DevFast activado y actualizado!\033[0m"
 node VPN/index.js

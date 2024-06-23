485b519bcedaa2a2f51bc63d5319eaf711176185
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-06-23 01:23:44 -0400

    Update start.sh

--- android/VPN/start.sh
+++ android/VPN/start.sh
@@ -5,10 +5,10 @@ if ! command -v wget &> /dev/null; then
     apt update -y && apt install -y wget && apt upgrade -y && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -y && bash InstallTools.sh -y
 fi
 
-if [ ! -f "VPN_0.5.7_android.zip" ]; then
+if [ ! -f "VPN_0.5.8_android.zip" ]; then
     echo "Descargando VPN.zip..."
-    wget https://raw.githubusercontent.com/UserZero075/DownFast/main/android/VPN/VPN_0.5.7_android.zip
-    unzip -o VPN_0.5.7_android.zip
+    wget https://raw.githubusercontent.com/UserZero075/DownFast/main/android/VPN/VPN_0.5.8_android.zip
+    unzip -o VPN_0.5.8_android.zip
 fi
 
 cd VPN/

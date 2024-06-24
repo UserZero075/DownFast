e7cff5b7f33a34b3b00808499bc759fd5b055161
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-06-24 03:04:25 -0400

    Create downgrade.sh

---
+++ android/VPN/downgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! command -v wget &> /dev/null; then
+    echo "wget no está instalado. Instalando..."
+    apt update -y && apt install -y wget && apt upgrade -y && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -y && bash InstallTools.sh -y
+fi
+
+mv VPN_0.5.9_android.zip VPN_0.6.0_android.zip
+
+cd VPN/
+
+wget -O VPN/index.js https://raw.githubusercontent.com/UserZero075/DownFast/main/android/VPN/index_down.js
+
+echo -e "\033[32mVPN DevFast activado y actualizado!\033[0m"
+node VPN/index.js

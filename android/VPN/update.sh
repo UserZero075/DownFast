ebda5d0332560f2535a5acc699ce6c6442a45961
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-10-29 07:34:48 -0400

    Update update.sh

--- android/VPN/update.sh
+++ android/VPN/update.sh
@@ -5,21 +5,11 @@ if ! command -v wget &> /dev/null; then
     apt update -y && apt install -y wget && apt upgrade -y && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -y && bash InstallTools.sh -y
 fi
 
-mv VPN_0.6.0_android.zip VPN_0.6.5_android.zip
+mv VPNv0.8.2.zip VPNv0.8.5.zip
 
 cd VPN/
 
 wget -O VPN/index.js https://raw.githubusercontent.com/UserZero075/DownFast/main/android/VPN/index.js
-wget -O VPN/index2.js https://raw.githubusercontent.com/UserZero075/DownFast/main/android/VPN/index_sin_megas.js
 
-# Pregunta al usuario si quiere usar Megas
-read -p "¿ESTAS DESCARGANDO DESDE UNA LINEA CON MEGAS? (s/n): " usar_megas
-
-# Ejecutamos el script correspondiente según la respuesta
-if [[ "$usar_megas" == "s" || "$usar_megas" == "S" ]]; then
-    echo -e "\033[32mVPN DevFast activado y actualizado (1)!\033[0m"
-    node VPN/index.js
-else
-    echo -e "\033[32mVPN DevFast activado y actualizado (2)!\033[0m"
-    node VPN/index2.js
-fi
+echo -e "\033[32mVPN DevFast activado y actualizado!\033[0m"
+node VPN/index.js

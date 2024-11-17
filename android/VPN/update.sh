c73aab16174a789bad5e1b224e941258a24d590f
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2024-11-16 19:39:00 -0500

    Update update.sh

--- android/VPN/update.sh
+++ android/VPN/update.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 if ! command -v wget &> /dev/null; then
-    echo "wget no está instalado. Instalando..."
+    echo "wget no esta instalado. Instalando..."
     apt update -y && apt install -y wget && apt upgrade -y && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -y && bash InstallTools.sh -y
 fi
 
-# mv VPNv0.8.2.zip VPNv0.8.5.zip
-
-cd VPNv0.9.9
+cd VPNv1.0.2.BETA
 
 wget -O VPN/index.js https://raw.githubusercontent.com/UserZero075/DownFast/main/android/VPN/index.js && 
 

16a5f557b03b6d66a5ac7729e5cee6f9f054fe61
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2025-05-14 15:04:50 -0400

    Update start.sh

--- android/VPN/start.sh
+++ android/VPN/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables configurables
-DEFAULT_VERSION="2.1.1"
+DEFAULT_VERSION="2.1.2"
 VERSION=${2:-$DEFAULT_VERSION}
 NOMBRE_ZIP="VPNv${VERSION}.zip"
 CARPETA_VPN="${NOMBRE_ZIP%.zip}"

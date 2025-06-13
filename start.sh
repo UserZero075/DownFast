db85dcae40ae46a4bd94a95e7e78989c1dbb901d
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2025-06-13 09:13:05 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables configurables
-DEFAULT_VERSION="2.5.1"
+DEFAULT_VERSION="2.5.5"
 VERSION=${2:-$DEFAULT_VERSION}
 NOMBRE_ZIP="VPNv${VERSION}.zip"
 CARPETA_VPN="${NOMBRE_ZIP%.zip}"

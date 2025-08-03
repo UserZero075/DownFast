46b3c8cdc607074782b79101f710b635b73ea3d8
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2025-08-03 14:32:15 -0400

    Update startPy.bash

--- startPy.bash
+++ startPy.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables configurables 
-DEFAULT_VERSION="_v0.7.1pyBeta"
+DEFAULT_VERSION="_v0.8pyBeta"
 VERSION=${2:-$DEFAULT_VERSION}
 NOMBRE_ZIP="DF_VPN-Down${VERSION}.zip"
 CARPETA_VPN="${NOMBRE_ZIP%.zip}"

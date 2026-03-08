46ddb5b0bc061795cd33e1131d52e1e9470019a2
UserZero075 <98463850+UserZero075@users.noreply.github.com>
2026-03-08 09:45:38 -0400

    Update startPyTest.bash

--- startPyTest.bash
+++ startPyTest.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables configurables 
-DEFAULT_VERSION="_v15.4.5.1pyBeta"
+DEFAULT_VERSION="_v15.5pyBeta"
 VERSION=${2:-$DEFAULT_VERSION}
 NOMBRE_ZIP="DF_VPN-Down${VERSION}.zip"
 CARPETA_VPN="${NOMBRE_ZIP%.zip}"
